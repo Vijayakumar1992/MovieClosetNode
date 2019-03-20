37b18ca23b3400cfb183eb3de06c6a991f1a8ecc
david aruldass <davidaruldass@yahoo.com>
2019-03-20 10:28:13 -0600

    add

--- public/script.js
+++ public/script.js
@@ -3,9 +3,11 @@ function loadDoc() {
     console.log("This project is working");
     //defines a function to be executed when the readyState property changes
     xhttp.onreadystatechange = function () {
+        console.log("in the callback function");
         if (this.readyState == 4 && this.status == 200) { // readyState/Status-holds the status of the XMLHttpRequest
 
             test(JSON.parse(this.responseText));
+            console.log("this is the response text" + this.responseText);
         }
     };
     //https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp -has a good explanation
@@ -19,8 +21,10 @@ let htmlPage = "";
 
 function test(response) {
     let length = response.length
+    
     for (let i = 0; i < length; ++i) {  
         htmlPage += "<p>" + response[i].type + "</p>" // add on it 
     }
-    document.getElementById("p").innerHTML = htmlPage;  //
+    document.getElementById("p").innerHTML = htmlPage;
+    console.log("were in test" + htmlPage);  //
 }
