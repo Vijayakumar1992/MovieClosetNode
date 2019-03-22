d61da6b642aea16058c4f75cc8eb95bd377cf75d
david aruldass <davidaruldass@yahoo.com>
2019-03-21 19:30:34 -0600

    add

--- public/script.js
+++ public/script.js
@@ -8,12 +8,12 @@ function loadDoc() {
         if (this.readyState == 4 && this.status == 200) { // readyState/Status-holds the status of the XMLHttpRequest
             console.log("this is the response text" + this.responseText);
 
-            var html = "<ul>";
+            //var html = "<ul>";
             var movieData = JSON.parse(this.responseText); // turns into js object
             console.log(movieData); // actually turning into js object
             console.log(movieData.rowCount);  // accessing js object
 
-            for( var i= 0; i < movieData.rowCount; i++)
+            for( var i= 0; i < movieData.rowCount; i++){
                     console.log(movieData.rows[i].movie_name);
             // <ul>
             //     <li>mov1</li>
@@ -22,6 +22,7 @@ function loadDoc() {
 
             //using movieList to display the movies in the front end page of html
             document.getElementById("movieList").innerHTML = html; 
+            }
         }
     };
     //https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp -has a good explanation
