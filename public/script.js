0b287d2a418a0443761da2f334ab6d3a0799feb2
davidaruldass <davidaruldass@yahoo.com>
2019-03-29 10:05:26 -0600

    add

--- public/script.js
+++ public/script.js
@@ -5,17 +5,20 @@ function getMovies(movieType) {
     {        
         if (this.readyState == 4 && this.status == 200) { // readyState/Status-holds the status of the XMLHttpRequest          
 
-            let html = "<ul class='movieList'>";
+            let html = "<select id='movies'>";
                 var movieData = JSON.parse(this.responseText); // turns text into js object
                 console.log(movieData); // actually turning into js object
                 console.log(movieData.length);  // accessing js object
 
                 for( var i= 0; i < movieData.length; i++){
                         console.log(movieData[i].movie_name);            
-                    html += "<li>" + movieData[i].movie_name + "</li>";               
+                    html += "<option>" + movieData[i].movie_name + "</option>";               
                 }           
-            html += "</ul>"; // closing ul  list
+            html += "</select>"; // closing ul  list
+            html += "<button onclick = 'edit()'> Edit</button>";
+            html += "<button onclick = 'deleteMovie()'> Delete</button>";
             //using movieList to display the movies in the front end page of html
+            document.getElementById("movieList").style.display = "block";
             document.getElementById("movieList").innerHTML = html; // takis html insert into html
         }
     }
@@ -23,3 +26,27 @@ function getMovies(movieType) {
     xhttp.open("GET", "/getMovies?type=" + movieType, true);  //Send a Request To a Server
     xhttp.send();
 }
+
+function edit() {
+    var txt;
+    var movie = document.getElementById("movies").value; // will get the movies entered by user      
+    var person = prompt("Edit the movie:", movie);
+    if (person == null || person == "") {
+      txt = "User cancelled the prompt.";
+    } else {
+      txt =  "This week we will watch:" + person;
+    }
+    document.getElementById("movieList").innerHTML = txt;
+  }
+
+  function deleteMovie() {
+    var txt;
+    var movie = document.getElementById("movies").value;   
+    var person = prompt("Delete the movie:", movie);
+    if (person == null || person == "") {
+      txt = "User cancelled the prompt.";
+    } else {
+      txt =  "This week we will watch:" + person;
+    }
+    document.getElementById("movieList").innerHTML = txt;
+  }
